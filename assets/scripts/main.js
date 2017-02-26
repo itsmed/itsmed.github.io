934cd9aeb2aa770f96cbd184a5e6781efc3ded32
DejonGill <dejonjgill@gmail.com>
2017-02-26 11:40:47 -0800

    nav mobile friendly

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
   checkWindow();
   $(window).resize(checkWindow);
+  $('#nav_burger').click(function() {
+    $('nav').toggleClass('hidden');
+  });
 });
 
 function checkWindow() {
   if (document.body.clientWidth <= 767) {
-    console.log('hello');
-  }
+    $('nav').addClass('hidden');
+    $('#nav_burger').removeClass('hidden');
+  } else {
+    $('#nav_burger').addClass('hidden');
+  } 
 }
